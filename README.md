2b439d7c49e35f29d8f1acc6533ba84467219001
oloruntomiojo <dieavid01@gmail.com>
2023-10-16 22:40:47 -0500

    starting files

---
+++ README.md
@@ -0,0 +1,21 @@
+# News Homepage - Frontend Mentor Challenge
+This is a solution to a challenge from Frontend Mentors. The solution can be found on my [Frontend Mentor profile](https://www.frontendmentor.io/solutions/responsive-news-page-with-nextjs-and-tailwindcss-Yfbm3i0W8Z). The live site can be found here -  [News homepage](https://webnews-homepage.vercel.app/).
+
+## Outline
+- [Overview](#overview)  
+- [Tools](#tools)
+- [Deployment](#deployment)
+
+### Overview
+The task given was to build the landing page for an insurance company using the designs provided in the project files. The landing page basically consists of 5 sections: header, hero, flow process, a banner and a footer. The data (text and images) were all provided by the designers. The original challenge and files related can be found [here](https://www.frontendmentor.io/challenges/insure-landing-page-uTU68JV8/hub). Below is the developed preview for the website:
+
+![Screenshot 2023-02-21 185115](https://user-images.githubusercontent.com/55490663/220492438-31962fb6-56d2-4989-b25b-af046d015489.png 'Challenge Preview')
+
+### Tools
+I used the following in developing this project:
+- HTML
+- Tailwindcss
+- Typescript
+
+### Deployment
+This project was deployed on [Vercel](https://vercel.com). Vercel is a deployment platform from the creators of Next.js.
---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ['./dist/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Karla', ...defaultTheme.fontFamily.sans],
+        heading: ['DM Serif Display', ...defaultTheme.fontFamily.serif],
+      },
+      colors: {
+        'primary-violet': 'hsl(256, 26%, 20%)',
+        'primary-blue': 'hsl(216, 30%, 68%)',
+        'secondary-dark-violet': 'hsl(270, 9%, 17%)',
+        'secondary-grayish-violet': 'hsl(273, 4%, 51%)',
+        'secondary-light-gray': 'hsl(0, 0%, 98%)',
+      },
+
+      backgroundImage: {
+        'nav-pattern': "url('/src/images/bg-pattern-mobile-nav.svg')",
+        'intro-pattern-left':
+          "url('/src/images/bg-pattern-intro-left-mobile.svg'), url('/src/images/bg-pattern-intro-right-mobile.svg')",
+        'work-pattern-mobile':
+          "url('/src/images/bg-pattern-how-we-work-mobile.svg')",
+        'footer-pattern-mobile':
+          "url('/src/images/bg-pattern-footer-mobile.svg')",
+      },
+
+      backgroundPosition: {
+        'intro-position': 'left top, right bottom',
+      },
+
+      fontSize: {
+        '2.5xl': ['1.75rem', '2.125rem'],
+        '4.5xl': ['2.5rem', 1],
+        '5.5xl': ['2.95rem', 1],
+      },
+    },
+  },
+  plugins: [],
+};
